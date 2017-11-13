834b104209c59e3f7fa17da1e1e0c841d007ed91
ValdVes <vccs@inbox.ru>
2017-11-14 01:18:47 +0700

    add coroutine

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
+require("babel-core/register");
+require("babel-polyfill");
 import url from 'url';
 import nodePath from 'path';
 import axios from 'axios';
 import fs from 'mz/fs';
+
+
 
 const makeFileName = (uri, dir) => {
   const { host, path } = url.parse(uri);
   const fileName = `${host}${path}`.slice(0, -1).replace(/[^\w]/g, '-');
   return `${dir}${nodePath.sep}${fileName}.html`;
 };
+
+const getResponseDataGen = function* () {
+  const url = yield;
+  const data = yield axios.get(url)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((httpErr) => {
+      throw new Error(httpErr);
+    });
+};
+
 
 export default (uri, outputPath) => {
   const directory = outputPath ? outputPath : __dirname;
@@ -17,17 +33,10 @@ export default (uri, outputPath) => {
   console.log('PATH GIVEN: ', outputPath);
   console.log("Page will be saved to: ", fullFileName);
 
-  axios.get(uri)
-    .then((response) => {
-      fs.writeFile(fullFileName, response.data).then((writeErr) => {
-        if (writeErr) {
-          throw new Error(writeErr);
-        }
-        console.log("Page downloaded successfully");
-      })
-    })
-    .catch((httpErr) => {
-      throw new Error(httpErr);
-    })
+  const dataGen = getResponseDataGen();
+  dataGen.next();
+  const { value } = dataGen.next(uri);
+  value.then(data => dataGen.next(data));
+
 
 };
