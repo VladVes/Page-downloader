b26d0d00047e04414a85d3bbc8b4513f3cd106dc
VladVes <vccs@inbox.ru>
2017-11-14 11:51:31 +0700

    add generator

--- src/index.js
+++ src/index.js
@@ -5,25 +5,36 @@ import nodePath from 'path';
 import axios from 'axios';
 import fs from 'mz/fs';
 
-
-
 const makeFileName = (uri, dir) => {
   const { host, path } = url.parse(uri);
   const fileName = `${host}${path}`.slice(0, -1).replace(/[^\w]/g, '-');
   return `${dir}${nodePath.sep}${fileName}.html`;
 };
 
-const getResponseDataGen = function* () {
-  const url = yield;
-  const data = yield axios.get(url)
+const getResponseData = (url) => {
+  return axios.get(url)
     .then((response) => {
       return response.data;
     })
     .catch((httpErr) => {
-      throw new Error(httpErr);
+      return new Error(httpErr);
     });
 };
 
+const writeToFile = (data, filenName) => {
+  return fs.writeFile(filenName, data)
+    .then(() => console.log('Page saved successfully'), (writeErr) => {
+      return new Error(writeErr);
+    });
+};
+
+const gen = function* (url, fileName) {
+  console.log("URL FROM GENERATOR: ", url);
+  console.log("FILENAME FROM GENERATOR: ", fileName);
+  const data = yield getResponseData(url);
+  //console.log("DATA: ", data);
+  yield writeToFile(data, fileName);
+};
 
 export default (uri, outputPath) => {
   const directory = outputPath ? outputPath : __dirname;
@@ -33,10 +44,21 @@ export default (uri, outputPath) => {
   console.log('PATH GIVEN: ', outputPath);
   console.log("Page will be saved to: ", fullFileName);
 
-  const dataGen = getResponseDataGen();
-  dataGen.next();
-  const { value } = dataGen.next(uri);
-  value.then(data => dataGen.next(data));
+  const coroutine = (generator) => {
+    const process = generator(uri, fullFileName);
+    const next = (result) => {
+      const value = resul.value;
+      if (result.done) {
+        return value;
+      }
+      value.then(
+        data => next(process.next(data)),
+        err => next(process.throw(err))
+      );
+    }
 
+    return next(process.next());
+  };
 
+  coroutine(gen);
 };
